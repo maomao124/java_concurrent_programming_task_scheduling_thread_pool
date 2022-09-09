e682b8573b2583a43e446c58ccd40632d9c87b9d
mao <1296193245@qq.com>
2022-09-09 20:10:13 +0800

    java_concurrent_programming_task_scheduling_thread_pool

---
+++ src/main/java/mao/t6/Test.java
@@ -0,0 +1,58 @@
+package mao.t6;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Project name(项目名称)：java并发编程_任务调度线程池
+ * Package(包名): mao.t6
+ * Class(类名): Test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/9/9
+ * Time(创建时间)： 20:07
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Test
+{
+    /**
+     * 日志
+     */
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args)
+    {
+        log.debug("开始运行");
+        ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(2);
+
+        for (int i = 0; i < 2; i++)
+        {
+            int finalI = i;
+            threadPool.scheduleWithFixedDelay(new Runnable()
+            {
+                @Override
+                @SuppressWarnings("all")
+                public void run()
+                {
+                    try
+                    {
+                        log.debug(finalI + "开始");
+                        int j = 1 / 0;
+                        log.debug(finalI + "结束");
+                    }
+                    catch (Exception e)
+                    {
+                        log.error("产生异常！异常内容：", e);
+                    }
+                }
+            }, 1, 2, TimeUnit.SECONDS);
+        }
+    }
+}
